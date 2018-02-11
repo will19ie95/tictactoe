d2a58c7d40d6ab81e40ba635f95d05529208f634
Yong Lei <will19ie95@gmail.com>
2018-02-10 22:43:29 -0500

    fixed

--- app.js
+++ app.js
@@ -40,81 +40,113 @@ app
     });
   });
 
-app.post("/play", function(req, res) {
+function check_winner(grid) {
+  var winner = " ";
 
-  var check_winner = function (grid) {
-    winner;
+  console.log("\nchecking board: ", grid);
 
+  if (grid[0] === "X" || grid[0] === "O") {
+    console.log("checking grid 0")
     // winning with 0
-    if (grid[0] === "X" || grid[0] === "O") {
-      if ((grid[0] === grid[1] && grid[1] === grid[2]) || (grid[0] === grid[3] && grid[3] === grid[6])) {
-        winner = grid[0];
-      }
+    if (
+      (grid[0] === grid[1] && grid[1] === grid[2]) ||
+      (grid[0] === grid[3] && grid[3] === grid[6])
+    ) {
+      winner = grid[0];
     }
 
+  }
+  
+  if (grid[4] === "X" || grid[4] === "O") {
     // winning with 4
-    if (grid[4] === "X" || grid[4] === "O") {
-      if ((grid[0] === grid[4] && grid[4] === grid[8]) || (grid[1] === grid[4] && grid[4] === grid[7]) || (grid[2] === grid[4] && grid[4] === grid[6]) || (grid[3] === grid[4] && grid[4] === grid[5])) {
-        winner = grid[4];
-      }
+    console.log("checking grid 4");
+    if (
+      (grid[0] === grid[4] && grid[4] === grid[8]) ||
+      (grid[1] === grid[4] && grid[4] === grid[7]) ||
+      (grid[2] === grid[4] && grid[4] === grid[6]) ||
+      (grid[3] === grid[4] && grid[4] === grid[5])
+    ) {
+      winner = grid[4];
     }
-
+    
+  }
+  
+  if (grid[8] === "X" || grid[8] === "O") {
     // winning with 8
-    if (grid[8] === "X" || grid[8] === "O") {
-      if ((grid[2] === grid[5] && grid[5] === grid[8]) || (grid[6] === grid[7] && grid[7] === grid[8])) {
-        winner = grid[8];
-      }
+    console.log("checking grid 8");
+    if (
+      (grid[2] === grid[5] && grid[5] === grid[8]) ||
+      (grid[6] === grid[7] && grid[7] === grid[8])
+    ) {
+      winner = grid[8];
     }
-
-    if (winner === "X" || winner === "O") {
-      console.log(winner + " Won");
-      app.grid = [" ", " ", " ", " ", "", " ", " ", " ", " "];
-    }
-
-    return winner;
   }
 
-  var winner = check_winner(app.grid);
+  console.log("winner is : " + winner);
 
-  console.log("Body Req:", req.body.grid);
+  return winner;
+};
 
+app.post("/play", function(req, res) {
+  
   // accept user move
   app.grid = req.body.grid;
 
-  if (winner) {
+  console.log("\nRequest \n\t", app.grid);
+
+  let winner = check_winner(app.grid);
+  let tie = false;
+
+  if (winner !== "O" && winner !== "X") {
+    canMove = false;
+
+    for (var i = 0; i < app.grid.length; i++) {
+      if (app.grid[i] !== "X" && app.grid[i] !== "O") {
+        canMove = true;
+      }
+    }
+    // place a move
+    if (canMove) {
+      // computer makes a random move
+      placing_move = true;
+
+      while (placing_move) {
+        let rand = Math.floor(Math.random() * 9);
+        if (app.grid[rand] !== "X" && app.grid[rand] !== "O") {
+          app.grid[rand] = "O";
+          placing_move = false;
+        }
+      }
+    } else {
+      // Tie, implement?
+      console.log(" Tie! ");
+      tie = true;
+    }
+  }
+
+  winner = check_winner(app.grid);
+  // if (winner === undefined) {
+  //   winner = " ";
+  // }
+
+  send_grid = app.grid.slice();
+
+  if (tie === true) {
+    winner = undefined;
+    console.log("/n/nTIE is detected s");
+    app.grid = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  } else if (winner === "X" || winner === "O") {
     app.grid = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
   }
 
-  move = -1;
-  canMove = false;
-
-  for (var i=0; i < app.grid.length; i++) {
-    if (app.grid[i] !== "X" && app.grid[i] !== "O") {
-      canMove = true;
-    }
-  }
-
+  console.log("WINNNING: ", winner);
   
-  if (winner !== "X" && winner !== "O" && canMove) {
-    // computer makes a random move
-    placing_move = true;
-    while (placing_move) {
-      let rand = Math.floor(Math.random() * 9);
-      if (app.grid[rand] !== "X" && app.grid[rand] !== "O") {
-        app.grid[rand] = "O";
-        // console.log("Placing move at tile: ", rand);
-        move = rand;
-        placing_move = false;
-      }
-    }
-  }
-
-
   data = {
-    grid: app.grid,
-    winner: winner,
-    computer_move: move
+    grid: send_grid,
+    winner: winner
   }
+
+  console.log("Sending \t", data)
 
   // users turn again
   res.json(data);
--- public/js/ttt.js
+++ public/js/ttt.js
@@ -1,84 +1,120 @@
-$(document).ready(function() {
+function get_curr_grid() {
+  var curr_grid = [];
 
-  function get_curr_grid () {
-        var curr_grid = [];
-
-        $(".tile").each(function() {
-          var self = $(this);
-          curr_grid.push(self.text());
-        });
-        return curr_grid;
-  }
-
-  function toggle_tile() {
-    $(".tile-").click(function() {
-      let tile_id = $(this).attr("id");
-
-      // place move front-end
-      let grid = get_curr_grid();
-
-      grid[tile_id] = "X";
-      $("#" + tile_id).html("X");
-      $("#" + tile_id)
-        .removeClass("tile-")
-        .addClass("tile-X");
-      $("#" + tile_id).unbind("click");
-
-      $.ajax({
-        url: "/play",
-        type: "post",
-        dataType: "json",
-        data: {
-          grid: grid
-        },
-        success: function(data) {
-          // place computer move front-end
-          computer_move = data.computer_move;
-          grid = data.grid;
-
-          $("#" + computer_move).html("O");
-          $("#" + computer_move).removeClass("tile-");
-          $("#" + computer_move).addClass("tile-O");
-          $("#" + computer_move).unbind("click");
-
-          winner = data.winner;
-
-          if (winner) {
-            // winner found
-            $("#alert-winner").html(winner + " Won!");
-            $("#alert-winner").css("display", "block");
-            // disconnect ui
-            $(".tile").unbind("click");
-          }
-        }
-      });
-
-
-    });
-  }
-
-  $("#restart-btn").click(function() {
-    var emptyGrid = ["", "", "", "", "", "", "", "", ""];
-    $(".tile-X").html("").addClass("tile-");
-    $(".tile-O").html("").addClass("tile-");
-
-    $(".tile").each(function() {
-      $(this).removeClass("tile-O");
-      $(this).removeClass("tile-X");
-      $(this).unbind("click");
-
-    })
-
-    toggle_tile();
-
-    $("#alert-winner").css("display", "none");
-
-    $.ajax({
-      url: "/",
-      type: "get",
-      dataType: "html"
-    });
+  $(".tile").each(function() {
+    var self = $(this);
+    curr_grid.push(self.text());
   });
+  return curr_grid;
+}
+
+function set_curr_grid(grid) {
+  // console.log("display grid", grid);
+
+  for (var i = 0; i < 9; i++) {
+    var li = $("#" + i);
+
+    li.html(grid[i]);
+
+    if (grid[i] === "O") {
+      li.unbind("click");
+      li.removeClass("tile-");
+      li.addClass("tile-O");
+    } else if (grid[i] === "X") {
+      li.unbind("click");
+      li.removeClass("tile-");
+      li.addClass("tile-X");
+    }
+  }
+}
+
+function show_winner(winner) {
+  // winner found
+  $("#alert-winner").html(winner + " Won!");
+  $("#alert-winner").css("display", "block");
+}
+
+function disable_tile() {
+  $(".tile").unbind("click");
+}
+
+function play(grid) {
+  $.ajax({
+    url: "/play",
+    type: "post",
+    dataType: "json",
+    data: {
+      grid: grid
+    },
+    success: function(data) {
+      // place computer move front-end
+      var grid = data.grid;
+      var winner = data.winner;
+
+      // console.log("winner: ", winner);
+      // console.log("is undef: ", winner === undefined)
+
+      set_curr_grid(grid);
+      
+      var tie = winner === undefined;
+
+      //check for winner
+      if(winner === "X" || winner === "O") {
+        show_winner(winner);
+        // disable ui
+        disable_tile();
+
+      } else if (tie) {
+        show_winner("NO ONE");
+        disable_tile();
+      }
+
+    }
+  });
+}
+
+function toggle_tile() {
+  $(".tile-").click(function() {
+    let tile_id = $(this).attr("id");
+
+    // place move front-end
+    let grid = get_curr_grid();
+    grid[tile_id] = "X";
+    $("#" + tile_id).html("X");
+    $("#" + tile_id)
+      .removeClass("tile-")
+      .addClass("tile-X");
+    $("#" + tile_id).unbind("click");
+
+    play(grid);
+    
+  });
+}
+
+function restart() {
+  var emptyGrid = ["", "", "", "", "", "", "", "", ""];
+  $(".tile-X")
+    .html("")
+    .addClass("tile-");
+  $(".tile-O")
+    .html("")
+    .addClass("tile-");
+
+  $(".tile").each(function() {
+    $(this).removeClass("tile-O");
+    $(this).removeClass("tile-X");
+    $(this).unbind("click");
+  });
+
+  toggle_tile();
+
+  $("#alert-winner").css("display", "none");
+
+  $.ajax({ url: "/", type: "get", dataType: "html" });
+}
+
+
+$(document).ready(function() {
 
   toggle_tile();
   
