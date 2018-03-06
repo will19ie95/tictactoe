a8180c24212ed75da9a632dc397fe09732c4bd3e
Yong Lei <will19ie95@gmail.com>
2018-03-06 15:17:39 -0500

    rabbit

--- router.js
+++ router.js
@@ -72,7 +72,6 @@ router.post("/listen", function(req, res) {
         console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
         // return the message
         if (msg) {
-          self.conn.close();
           return res.json({
             status: "OK",
             msg: msg.content.toString()
